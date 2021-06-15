fa2541c66f65ee5194721733d88f6047135e8195
Ali ÇİÇEK <m.alicicek18@hotmail.com>
2021-06-16 00:44:43 +0300

    initial commit

---
+++ ProducerConsumerRabbitMQ/Sender.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ProducerConsumerRabbitMQ
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string msg = "Getting started with .Net Core RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(msg);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Sent message {0}...", msg);
+            }
+            Console.WriteLine("Press [enter] to exit the SenderApp");
+            Console.ReadLine();
+        }
+    }
+}
